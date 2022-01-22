bebae894216569974e32de5e823391bb9168b1d9
vanessawin <vanessa.win@hotmail.com>
2022-01-22 04:52:50 -0300

    ajustando responsividade

--- Teste para desenvolvedor web/appp.js
+++ Teste para desenvolvedor web/appp.js
@@ -2,32 +2,29 @@ function validar() {
     // pegando o valor  do campo userneme pelo id
     var usuario = document.getElementById("user").value
     var passowrd = document.getElementById("password").value
-    var lembrar = document.getElementById("lembrar")
-
+    var lembrar = document.getElementById("lembrar").checked
+    
+    
     // verificando se o campo esta preenchido
     if (!usuario) {
         //se o campo nao estiver preenchido
         alert('Preecha o campo Username')
 
-        return
+        return 
 
-    }
-    if (!passowrd) {
+    } else if (!passowrd) {
         alert('Preecha o passoword')
 
-        return
-    }
-
-    if (lembrar.checked) {
-        console.log("O cliente marcou o checkbox");
-    } else {
-        console.log("O cliente não marcou o checkbox");
-    }
-
+        return 
+    } 
 }
-if (usuario & passowrd === true){
-    var btn = document.querySelector('button');
-    btn.addEventListener('click', function() {
-    window.open ("https://www.showdeimagem.com.br ", " _blank")
-})
-}
+
+// ao clicar no btn redirecionar para outra pagina
+// obs: preciso fazer isso depois que passar pela validação de  usuario e seha
+var btn = document.querySelector('button');
+         btn.addEventListener('click', function () {
+            window.open("https://www.showdeimagem.com.br ", " _blank")
+        })
+    
+
+
