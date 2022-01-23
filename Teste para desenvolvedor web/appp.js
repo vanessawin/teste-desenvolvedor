531eb028f453111269ace9833a0f525abf33886d
vanessawin <vanessa.win@hotmail.com>
2022-01-23 01:28:13 -0300

    modificando

--- Teste para desenvolvedor web/appp.js
+++ Teste para desenvolvedor web/appp.js
@@ -3,28 +3,30 @@ function validar() {
     var usuario = document.getElementById("user").value
     var passowrd = document.getElementById("password").value
     var lembrar = document.getElementById("lembrar").checked
-    
-    
+
+       
+
+
     // verificando se o campo esta preenchido
     if (!usuario) {
         //se o campo nao estiver preenchido
         alert('Preecha o campo Username')
 
-        return 
+        return false
 
     } else if (!passowrd) {
         alert('Preecha o passoword')
 
-        return 
-    } 
+        return false
+    }
+
+    
 }
+
+    
+
 
 // ao clicar no btn redirecionar para outra pagina
 // obs: preciso fazer isso depois que passar pela validação de  usuario e seha
-var btn = document.querySelector('button');
-         btn.addEventListener('click', function () {
-            window.open("https://www.showdeimagem.com.br ", " _blank")
-        })
-    
 
 
