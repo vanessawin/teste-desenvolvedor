26a03abab23143ca4a6fac34dd1e2965f6f84d26
vanessawin <vanessa.win@hotmail.com>
2022-01-26 20:14:06 -0300

    validação e redirecionamento da pagina concludo com sucesso

--- Teste para desenvolvedor web/appp.js
+++ Teste para desenvolvedor web/appp.js
@@ -4,33 +4,35 @@ function validar() {
     // pegando o valor  do campo userneme pelo id
     var usuario = document.getElementById("user").value
     var passowrd = document.getElementById("password").value
-    var lembrar = document.getElementById("lembrar")
+    var lembrar = document.getElementById("lembrar")// falta resolver o campo para salvar senha
 
-       
+
 
 
     // verificando se o campo esta preenchido
     if (!usuario) {
-        //se o campo nao estiver preenchido
+        //se o campo usuario não estiver preenchido abra um alerta para o usuario
         alert('Preecha o campo Username')
 
         return false
 
+        //se o campo password não estiver preenchido abra um alerta para o usuario
     } else if (!passowrd) {
         alert('Preecha o passoword')
 
         return false
-    }else {
-       window.location.assign("https://www.w3schools.com")
-    }
 
-    Btn.click()
+      // se todos os campos acima estiverem preenchido   
+    } else {
+        // ao clicar no btn redirecionar para outra pagina
+        window.open("https://www.showdeimagem.com.br ", " _blank")
+    }
 }
 
-    
 
 
-// ao clicar no btn redirecionar para outra pagina
+
+
 // obs: preciso fazer isso depois que passar pela validação de  usuario e seha
 
 
